9f6266f1dd7faa4e39707dc29d82ba138d7b83db
vsavchenko-13 <v.savchenko1304@gmail.com>
2024-10-25 20:55:05 -0600

    Added some logic, css and html to start building out the page
    Trying to make separate the functions as per SOLID, otherwise just reiterating through text

--- src/index.js
+++ src/index.js
@@ -1,7 +1,39 @@
-import "./styles.css";
-const firstHeader = document.querySelector("h1");
-console.log("hai");
+import * as css from "./styles.css";
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const menus = document.querySelector(".menus");
+const addProject = document.querySelector(".addProject");
+//addProject.addEventListener("click", newProject);
 
-const randomText = document.createElement("h3");
-randomText.textContent = "Just some random text";
-firstHeader.appendChild(randomText);
+
+function createElement(attributes){
+      console.log(attributes.name);
+}
+
+createElement({name:"bib", age:"56",hobbies:["games","drugs","food"]})
+
+
+/*function newProject(){
+    const newForm = document.createElement("form");
+    const label = document.createElement("label");
+    label.htmlFor = "projectName";
+    label.innerText = "Project Name:"
+    
+    const projectName = document.createElement("input");
+    projectName.type="text";
+    projectName.id = "projectName";
+    projectName.name = "projectName";
+
+    const button = document.createElement("button");
+    button.type = "submit";
+    button.value = "Submit"
+    button.textContent = "Submit";
+
+    newForm.appendChild(label);
+    newForm.appendChild(projectName);
+    newForm.appendChild(button);
+    menus.appendChild(newForm);
+}*/
+
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -7,14 +7,19 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'index.js',
     },
+    
     module: {
         rules: [
             { test: /\.txt$/, use: 'raw-loader' },
-            { test: /\.css$/, use: 'css-loader' },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+              },
             { test: /\.html$/, use: 'html-loader' },
         ],
     },
     plugins: [new HtmlWebpackPlugin({
         template: './src/template.html',
     })],
+    mode: 'development'
 };
